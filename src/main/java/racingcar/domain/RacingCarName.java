bf86c8eb4eafadbc4b7d4687dc523a2a4c413651
noelleseo <noelleseo@gmail.com>
2022-10-10 08:51:43 +0900

    feat: 자동차 이름 객체 추가

---
+++ src/main/java/racingcar/domain/RacingCarName.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+import racingcar.util.Constants;
+import racingcar.util.RegexUtil;
+
+public class RacingCarName {
+    private String name;
+
+    public RacingCarName(String name) {
+        validateName(name.trim());
+        this.name = name.trim();
+    }
+    
+    public String toString() {
+        return name;
+    }
+    
+    private void validateName(String name) {
+        if(!RegexUtil.match("^.{1,5}$", name)) {
+            throw new IllegalArgumentException(Constants.ERR_1_TO_5_CHAR);
+        }
+    }
+}
