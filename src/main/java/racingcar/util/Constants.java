d0a2aa91fe02678203c2a77f7ff1ec73d24f125a
noelleseo <noelleseo@gmail.com>
2022-10-10 08:39:28 +0900

    feat: 필요 메시지 상수로 정리하여 유틸화

---
+++ src/main/java/racingcar/util/Constants.java
@@ -0,0 +1,15 @@
+package racingcar.util;
+
+public class Constants {
+    public static String MSG_GET_ENTRY = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static String MSG_GET_ROUND = "시도할 회수는 몇회인가요?";
+    public static String MSG_WINNER = "최종 우승자";
+    
+    public static String E_COMMA = ",";
+    public static String E_RESULT = "실행 결과";
+    public static String E_COLON = ":";
+    public static String E_HYPHEN = "-";
+    
+    public static String ERR_1_TO_5_CHAR = "[ERROR] 자동차 이름은 1자에서 5자 사이로 입력해 주세요.";
+    public static String ERR_NOT_NUMBER = "[ERROR] 시도 회수는 숫자여야 합니다.";
+}
